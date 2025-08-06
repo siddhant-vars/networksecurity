413a41e19fa6169282d3405d510cfa9bb1b1cdec
siddhant-vars <siddhantvarshney446@gmail.com>
2025-08-06 12:04:52 +0530

    Data Ingestion component completed

---
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import pandas as pd
+import numpy as np
+
+"""
+defining common constant for training pipeline
+"""
+TARGET_COLUMN="Result"
+PIPELINE_NAME:str="NetworkSecurity"
+ARTIFACT_DIR:str="Artifacts"
+FILE_NAME:str="phisingData.csv"
+
+TRAIN_FILE_NAME:str="train.csv"
+TEST_FILE_NAME:str="test.csv"
+
+"""
+data ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME:str="NetworkData"
+DATA_INGESTION_DATABASE_NAME:str="SiddhantAI"
+DATA_INGESTION_DIR_NAME:str="data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR:str="feature_store"
+DATA_INGESTION_INGESTED_DIR:str="ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION:float=0.2
